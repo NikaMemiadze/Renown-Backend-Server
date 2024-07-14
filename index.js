0ffe38101b4c88181fb32d110586f029e53e3c53
NikaMemiadze <123237771+NikaMemiadze@users.noreply.github.com>
2024-07-14 22:20:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const authRouter = require('./routes/authRoute')
+const profileRouter = require('./routes/profileRoute')
 const cors = require('cors')
 const app = express()
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/renown')

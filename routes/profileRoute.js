de2907bcc95c17c9ca38a32f2fab5374deb434f0
NikaMemiadze <123237771+NikaMemiadze@users.noreply.github.com>
2024-07-15 15:07:43 +0400

    Update profileRoute.js

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const profileController = require('../controllers/profileController')
+const profileController = require('../controllers/profileController');
+const { authenticateToken } = require('../security/authenticateToken');
+const { globalLimiter } = require('../security/rateLimiter');
 const router = express.Router();
 
-router.post('/deleteAccount', profileController.authenticateToken, profileController.deleteAccount);
+router.use(globalLimiter); 
+router.use(authenticateToken); 
+
+router.post('/deleteAccount', profileController.deleteAccount);
+router.post('/changeEmail', profileController.changeEmail);
+router.post('/changePassword', profileController.changePassword);
+router.post('/changeName', profileController.changeName);
 
 module.exports = router;
